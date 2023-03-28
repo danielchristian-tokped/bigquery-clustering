608079ce50ea603c08d9b99565e9288720db205b
Daniel Chistian Pradipta <daniel.christian@tokopedia.com>
2023-03-28 16:48:06 +0700

    Add Update for spreadsheet

---
+++ constant/constant.go
@@ -0,0 +1,26 @@
+package constant
+
+// System Related
+const (
+	CLUSTER_ENV    = "CLUSTERENV"
+	CredentialPath = "/credentials/"
+	PrefixFileSA   = "clustering-service-account"
+
+	LogPath = "/var/log/tokopedia/bigquery-cluster/bigquery-cluster"
+
+	FatalLogName = "error-fatal.log"
+	ErrorLogName = "error.log"
+	InfoLogName  = "info.log"
+)
+
+// Env
+const (
+	PRODUCTION = "production"
+	STAGING    = "staging"
+)
+
+// Sheet
+const (
+	NONE        = "NONE"
+	WorksheetID = "Sheet1"
+)
